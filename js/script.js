d304792b3c91f97b7f0c8d321d7216310410793f
janjani <rifqijanjani666@gmail.com>
2023-12-31 09:41:14 +0700

    modified:   index.html 	modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 {/* <>// MathJax-Script
     <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
     <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script></> */}
-
+// Area of Triangle Calculation
 const form = document.querySelector("form");
 const baseInput = document.getElementById("base");
 const heightInput = document.getElementById("height");
 const equationElement = document.getElementById("equation");
+const calculationElement = document.getElementById("calculation");
 const resultElement = document.getElementById("result");
 
 form.addEventListener("submit",(event) => {
@@ -17,7 +18,8 @@ form.addEventListener("submit",(event) => {
 
     // Update equation
     const equationString = `Equation: A = 1/2 x ${base} x ${height}`;
-
+    
+    // Showing the equation after clicking submit
     equationElement.textContent = equationString;
     equationElement.style.display = "block";
 
@@ -28,4 +30,10 @@ form.addEventListener("submit",(event) => {
     resultElement.textContent = `Result: A = ${area.toFixed(2)}`;
 
     resultElement.style.display = "block";
+
+    // Calculation
+    const calculationString = `$$A = \\frac{1}{2} \times ${base} \times ${height} = ${area}$$`;
+
+    calculationElement.textContent = calculationString;
+    calculationElement.style.display = "block";
 })
