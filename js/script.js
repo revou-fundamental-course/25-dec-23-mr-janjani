8f0fbd042d868b9dbf70aed97dddb17c2d175858
janjani <rifqijanjani666@gmail.com>
2024-01-03 00:09:03 +0700

    Changes to be committed: 	modified:   css/style.css 	modified:   index.html 	modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
-{/* <>// MathJax-Script
-    <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
-    <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script></> */}
-// Area of Triangle Calculation
+    // Dropdown Function
+function toogleDropdown(id) {
+    var dropdown = document.getElementById(id);
+    if (dropdown.style.display === "none") {
+        dropdown.style.display = "block";
+        dropdown.scrollIntoView({behavior: "smooth", block: "end"})
+    } else {
+        dropdown.style.display = "none";
+    }
+}
+
+    // Area of Triangle Calculation
 const form = document.querySelector("form");
 const baseInput = document.getElementById("base");
 const heightInput = document.getElementById("height");
@@ -17,7 +25,7 @@ form.addEventListener("submit",(event) => {
     const base = parseFloat(baseInput.value);
     const height = parseFloat(heightInput.value);
 
-        // Calculate the area
+    // Calculate the area
     const area = (base * height ) / 2;
 
     // // Warning Message
@@ -54,4 +62,46 @@ form.addEventListener("submit",(event) => {
     // calculationElement.style.display = "block";
 
     // MathJax.Hub.Queue(["Typeset", MathJax.Hub, "calculationElement"]); // Trigger MathJax re-rendering
+})
+
+    // Perimeter of Triangle Calculation
+const side1Input = document.getElementById("side1");
+const side2Input = document.getElementById("side2");
+const side3Input = document.getElementById("side3");
+const equatPeri = document.getElementById("equationPerimeter");
+const calcPeri = document.getElementById("calculationPerimeter");
+const resultPeri = document.getElementById("resultPerimeter");
+const buttonPeri = document.getElementById("calculatePerimeter");
+    
+    // Attach click event to button for perimeter
+buttonPeri.addEventListener("click", function(event) {
+    event.preventDefault(); //Prevent the page to reload
+
+    // Get input values
+    const side1 = parseFloat(side1Input.value);
+    const side2 = parseFloat(side2Input.value);
+    const side3 = parseFloat(side3Input.value);
+
+    // Calculate the perimeter
+    const peri = side1 + side2 + side3;
+
+    // Alert Box
+    if (isNaN(peri)) {
+        alert("Please enter valid number for each sides of triangle");
+        return; //Prevent further execution
+    }
+
+    // Update equation
+    const calcPeriString = `P = ${side1} + ${side2} + ${side3}`;
+
+        // Showing the equation afyer clicking calculate
+    calcPeri.textContent = calcPeriString;
+    calcPeri.style.display = "block";
+    
+    equatPeri.style.display = "block";
+
+    // Update the result perimeter
+    resultPeri.textContent = `P = ${peri.toFixed(2)}`;
+    resultPeri.style.display = "block";
+    resultPeri.scrollIntoView({behavior: "smooth"});
 })
